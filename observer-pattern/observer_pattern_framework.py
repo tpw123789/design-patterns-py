6f1d5358dac2ffec256045d16b4dc4a0316d1b62
tpw123789 <tpw123789@gmail.com>
2021-10-09 03:51:16 +0800

    learning observer pattern framework

---
+++ observer-pattern/observer_pattern_framework.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Observer(metaclass=ABCMeta):
+    """觀察者的基類別"""
+    @abstractmethod
+    def update(self, observer, num):
+        pass
+
+
+class Observable:
+    """被觀察者的基類別"""
+    def __init__(self):
+        self._observer = []
+
+    def add_observer(self, observer):
+        self._observer.append(observer)
+
+    def remove_observer(self, observer):
+        self._observer.remove(observer)
+
+    def notify_observer(self, num=0):
+        for o in self._observer:
+            o.update(self, num)
+
+
+
