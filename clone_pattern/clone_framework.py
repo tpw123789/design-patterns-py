a1e2d759a0ab2b95cb3d75e4d598a0d83afbf652
tpw123789 <tpw123789@gmail.com>
2021-10-11 18:19:08 +0800

    clone framework

---
+++ clone_pattern/clone_framework.py
@@ -0,0 +1,44 @@
+from copy import copy, deepcopy
+
+
+class Clone:
+    """Clone 基類別"""
+    def clone(self):
+        """淺拷貝clone物件"""
+        return copy(self)
+
+    def deep_clone(self):
+        """深拷貝clone物件"""
+        return deepcopy(self)
+
+
+class Person(Clone):
+    """人"""
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def show_myself(self):
+        print(f'I am {self._name}, {self._age} years old')
+
+    def coding(self):
+        print(f'{self._name} coding now.')
+
+    def reading(self):
+        print('I am reading now.')
+
+    def sleeping(self):
+        print('I am sleeping now.')
+
+
+tony = Person('Tony', 10)
+tony.show_myself()
+tony.coding()
+tony.reading()
+tony.sleeping()
+
+tony1 = tony.clone()
+tony1.show_myself()
+tony1.coding()
+tony1.reading()
+tony1.sleeping()
